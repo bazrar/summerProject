c7f0c3e24809c16cb79f210011d860db91468c32
rejan bajracharya <rejanbajracharya@bajratechnologies.com>
2021-06-09 08:54:53 +0545

    routes

---
+++ routes/shop.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const router = require('express').Router()
+
+const rootDir = require('../utils/path')
+
+
+router.get('/', (req,res,next) => {
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+});
+
+module.exports = router;
